7fb42f727656975a29caae5f1a14a2aaf914feb3
Naresh Rayapati <naresh.rayapati@yahoo.com>
2019-07-17 15:24:21 -0500

    Update GrabTest.groovy

--- src/org/kcdc/GrabTest.groovy
+++ src/org/kcdc/GrabTest.groovy
@@ -3,7 +3,10 @@ package org.kcdc
 @Grab(group='org.apache.commons', module='commons-lang3', version='3.4')
 import org.apache.commons.lang3.SystemUtils
 
-def printInfo() {
+def printInfo(script) {
+    script.node {
+        println "hello"
+    }
     println 'We are ready to use packages from commons'
     if (SystemUtils.isJavaVersionAtLeast(8)) {
         println 'And there we go, and on Java 8'
